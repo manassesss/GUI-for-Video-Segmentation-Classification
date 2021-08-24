3c29d57bd688b49c22cc50e0145599fcbc1ab4da
manasses.silva <manasses3mm@gmail.com>
2021-08-24 11:22:42 -0300

    change: changing the base file

--- Interface.py
+++ Interface.py
@@ -1,8 +1,28 @@
 '''
 Base file for the interface.
 '''
-
 import tkinter as tk
+from tkinter import filedialog
+
+
+class MainWindow(root):
+    
+    def __init__(self):
+        pass
+    def openFile () :
+        window.filename = filedialog.askopenfilename()
+
+
+
 
 window = tk.Tk()
+menubar = tk.Menu(window)
+
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open...")
+filemenu.add_separator()
+
+menubar.add_cascade(label="File", menu=filemenu)
+
+window.config(menu=menubar)
 window.mainloop()
