b4a5ee6bb42d9e4b2210aedb33854d0a054bda8e
manasses.silva <manasses3mm@gmail.com>
2021-08-25 11:48:25 -0300

    change: organizing in Classes

--- Interface.py
+++ Interface.py
@@ -2,27 +2,27 @@
 Base file for the interface.
 '''
 import tkinter as tk
+import tkinter.messagebox as msg
 from tkinter import filedialog
 
 
-class MainWindow(root):
-    
-    def __init__(self):
+class MainWindow(tk.Tk):
+    def __init__(self,):
+        super().__init__()
+        self.geometry("800x600")
+        self.window = tk.Tk()
+        self.menubar = tk.Menu(self.window)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Open", command=self.openFile, accelerator="Ctrl+O")
+        self.filemenu.add_command(label="Save", command=self.saveFile, accelerator="Ctrl+S")
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.configure(menu=self.menubar)
+        
+    def openFile(self):
+        self.window.filename = filedialog.askopenfilename()
+    def saveFile(self):
         pass
-    def openFile () :
-        window.filename = filedialog.askopenfilename()
-
-
-
-
-window = tk.Tk()
-menubar = tk.Menu(window)
-
-filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open...")
-filemenu.add_separator()
-
-menubar.add_cascade(label="File", menu=filemenu)
-
-window.config(menu=menubar)
-window.mainloop()
+        
+if __name__ == "__main__":
+    mainwindow = MainWindow()
+    mainwindow.mainloop()
